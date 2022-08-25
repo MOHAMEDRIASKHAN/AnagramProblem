40a99cd5398734db3bd080649cd4af3720f498ac
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-25 21:15:39 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace AnagramProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "heart";
+            string str2 = "earth";
+
+            char[] chr1 = str1.ToLower().ToCharArray();
+            char[] chr2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(chr1);
+            Array.Sort(chr2);
+
+            string val1 = new string(chr1);
+            string val2 = new string(chr2);
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+        }
+    }
+}
