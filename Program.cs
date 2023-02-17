30944c61c2ee35ec8c822b3f8fd5628001fa9651
MOHAMEDRIASKHAN <108645671+MOHAMEDRIASKHAN@users.noreply.github.com>
2023-02-17 10:37:30 +0530

    Check_Anagram_or_not

--- Program.cs
+++ Program.cs
@@ -3,24 +3,57 @@
 
 namespace AnagramProblem
 {
-    class program
+    public class AnagramDetection
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+       
+        Console.Write("Enter first string: ");
+        string str1 = Console.ReadLine();
+        Console.Write("Enter second string: ");
+        string str2 = Console.ReadLine();
+
+        // Check if the strings are anagrams
+        bool Anagrams = CheckIfAnagrams(str1, str2);
+
+        // Print the result
+        if (Anagrams)
         {
-            string str1 = "heart";
-            string str2 = "earth";
-
-            char[] chr1 = str1.ToLower().ToCharArray();
-            char[] chr2 = str2.ToLower().ToCharArray();
-
-            Array.Sort(chr1);
-            Array.Sort(chr2);
-
-            string val1 = new string(chr1);
-            string val2 = new string(chr2);
-
-            Console.WriteLine(val1);
-            Console.WriteLine(val2);
+            Console.WriteLine("The two strings are anagrams.");
+        }
+        else
+        {
+            Console.WriteLine("The two strings are not anagrams.");
         }
     }
+
+    public static bool CheckIfAnagrams(string str1, string str2)
+    {
+        str1 = str1.Replace(" ", "").ToLower();
+        str2 = str2.Replace(" ", "").ToLower();
+
+        // Check if the strings have the same length
+        if (str1.Length != str2.Length)
+        {
+            return false;
+        }
+
+        char[] arr1 = str1.ToCharArray();
+        char[] arr2 = str2.ToCharArray();
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+
+        // Compare the sorted arrays
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 }
